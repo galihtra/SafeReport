97df42557ec08599d10f64f2d6e1250c392296d6
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-05-10 23:54:30 +0700

    update di home untuk detail user

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,19 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:safe_report/screens/signin_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  Future<DocumentSnapshot> getUserData() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      return FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+    } else {
+      throw ("No user logged in");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,8 +51,28 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(
-        child: Text('Welcome to Home Screen!'),
+      body: FutureBuilder<DocumentSnapshot>(
+        future: getUserData(),
+        builder: (BuildContext context,
+            AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Nama: ${data['name']}"),
+                  Text("Email: ${data['email']}"),
+                  Text("Gender: ${data['gender']}"),
+                ],
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.none) {
+            return Text("No user logged in");
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
       ),
     );
   }
