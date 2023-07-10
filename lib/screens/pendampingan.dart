14dbd11282a99466156a24a43b80ff0ff6600b9a
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-07-10 11:08:19 +0700

    perbaiki action ke halaman pelaporan dan pendampingan

---
+++ lib/screens/pendampingan.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Pendampingan extends StatelessWidget {
+  const Pendampingan({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Pendampingan")),
+    );
+  }
+}
