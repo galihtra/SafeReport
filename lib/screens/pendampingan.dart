411f45c5464a0046a6e7a6df45e7445f1c1cf111
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-07-12 22:27:59 +0700

    add detail pendampingan

--- lib/screens/pendampingan.dart
+++ lib/screens/pendampingan.dart
@@ -42,60 +42,69 @@ class _PendampinganState extends State<Pendampingan> {
             child: ListView(
               children: snapshot.data!.docs.map((doc) {
                 Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-                return ListTile(
-                  leading: (data['image_url'] != null)
-                      ? CircleAvatar(
-                          radius:
-                              25, // Setengah dari ukuran yang diinginkan (50/2)
-                          backgroundImage: NetworkImage(data['image_url']),
-                        )
-                      : CircleAvatar(
-                          radius:
-                              25, // Setengah dari ukuran yang diinginkan(50/2)
-                          child: Icon(
-                            Icons.person,
-                            color: Colors.white,
+                return InkWell(
+                  onTap: () {
+                    // Navigasi ke halaman detail item
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailPendamping(data: data),
+                      ),
+                    );
+                  },
+                  child: ListTile(
+                    leading: (data['image_url'] != null)
+                        ? CircleAvatar(
+                            radius: 25,
+                            backgroundImage: NetworkImage(data['image_url']),
+                          )
+                        : CircleAvatar(
+                            radius: 25,
+                            child: Icon(
+                              Icons.person,
+                              color: Colors.white,
+                            ),
+                            backgroundColor: Color(0xFFEC407A),
                           ),
-                          backgroundColor: Color(0xFFEC407A),
-                        ),
-                  title: Padding(
-                    padding: EdgeInsets.only(bottom: 1),
-                    child: Text(
-                      data['name'] ?? '',
-                      style: GoogleFonts.poppins(
-                          fontSize: 16, fontWeight: FontWeight.w500),
+                    title: Padding(
+                      padding: EdgeInsets.only(bottom: 1),
+                      child: Text(
+                        data['name'] ?? '',
+                        style: GoogleFonts.poppins(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
                     ),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 2),
-                        child: Text(
-                          data['gender'] ?? '',
-                          style: GoogleFonts.poppins(
-                              fontSize: 14, fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 4),
-                        child: Text(
-                          '(${data['prodi'] ?? ''})',
-                          style: GoogleFonts.poppins(
-                            fontSize: 12,
-                            fontWeight: FontWeight.normal,
-                            color: Color(0xFF98A2B3),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 2),
+                          child: Text(
+                            data['gender'] ?? '',
+                            style: GoogleFonts.poppins(
+                                fontSize: 14, fontWeight: FontWeight.w500),
                           ),
                         ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 8),
+                          child: Text(
+                            '(${data['prodi'] ?? ''})',
+                            style: GoogleFonts.poppins(
+                              fontSize: 12,
+                              fontWeight: FontWeight.normal,
+                              color: Color(0xFF98A2B3),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                        size: 14,
                       ),
-                    ],
-                  ),
-                  trailing: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.grey,
-                      size: 14,
                     ),
                   ),
                 );
@@ -103,6 +112,64 @@ class _PendampinganState extends State<Pendampingan> {
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class DetailPendamping extends StatelessWidget {
+  final Map<String, dynamic> data;
+
+  const DetailPendamping({required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text(''),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 300.0,
+              width: MediaQuery.of(context)
+                  .size
+                  .width, // Lebar sesuai ukuran layar
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: (data['image_url'] != null)
+                      ? NetworkImage(data['image_url'])
+                      : AssetImage('assets/images/default_avatar.png')
+                          as ImageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              data['name'] ?? '',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              data['gender'] ?? '',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '(${data['prodi'] ?? ''})',
+              style: TextStyle(
+                fontSize: 14,
+                color: Color(0xFF98A2B3),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
