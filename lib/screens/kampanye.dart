9016d3f676c1782eeabfd88c2afd9fd6fedd5506
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-07-20 09:55:00 +0700

    revisi detail kampanye untuk user

--- lib/screens/kampanye.dart
+++ lib/screens/kampanye.dart
@@ -9,162 +9,6 @@ import 'package:safe_report/model/user_model.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-class AdminKampanye extends StatefulWidget {
-  const AdminKampanye({Key? key});
-
-  @override
-  _AdminKampanyeState createState() => _AdminKampanyeState();
-}
-
-class _AdminKampanyeState extends State<AdminKampanye> {
-  final TextEditingController _judulController = TextEditingController();
-  final TextEditingController _deskripsiController = TextEditingController();
-  File? _selectedImage;
-
-  void _buatKampanye() async {
-    final String judul = _judulController.text;
-    final String deskripsi = _deskripsiController.text;
-
-    final User? user = FirebaseAuth.instance.currentUser;
-    final String adminId = user?.uid ?? '';
-
-    // Path lokal file gambar
-    File imageFile = _selectedImage!;
-
-    if (imageFile != null) {
-      final String imageName =
-          DateTime.now().millisecondsSinceEpoch.toString() + '.jpg';
-      final Reference storageReference = FirebaseStorage.instance
-          .ref()
-          .child('campaign_images')
-          .child(imageName);
-
-      final TaskSnapshot taskSnapshot =
-          await storageReference.putFile(imageFile);
-      final imageUrl = await taskSnapshot.ref.getDownloadURL();
-
-      final Campaign newCampaign = Campaign(
-        id: '',
-        title: judul,
-        description: deskripsi,
-        adminId: adminId,
-        participants: [],
-        imageUrl: imageUrl,
-      );
-
-      final CollectionReference campaignsRef =
-          FirebaseFirestore.instance.collection('campaigns');
-
-      final DocumentReference docRef =
-          await campaignsRef.add(newCampaign.toJson());
-      final String campaignId = docRef.id;
-
-      newCampaign.id = campaignId;
-      await docRef.update({'id': campaignId});
-
-      Navigator.pop(context);
-    }
-  }
-
-  Future<void> _ambilGambar() async {
-    final picker = ImagePicker();
-    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
-
-    if (pickedImage != null) {
-      setState(() {
-        _selectedImage = File(pickedImage.path);
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: Text(
-          "Buat Kampanye",
-          style: GoogleFonts.inter(
-            color: Colors.black,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        iconTheme: IconThemeData(color: Colors.black),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Judul',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextFormField(
-              controller: _judulController,
-              decoration: InputDecoration(
-                hintText: 'Masukkan judul kampanye',
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Deskripsi',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextFormField(
-              controller: _deskripsiController,
-              decoration: InputDecoration(
-                hintText: 'Masukkan deskripsi kampanye',
-              ),
-              maxLines: 4,
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Pilih Gambar',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            InkWell(
-              onTap: _ambilGambar,
-              child: _selectedImage != null
-                  ? Image.file(
-                      _selectedImage!,
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    )
-                  : Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.grey,
-                      child: Icon(
-                        Icons.image,
-                        color: Colors.white,
-                      ),
-                    ),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _buatKampanye,
-              child: Text('Buat Kampanye'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class ListKampanye extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -192,20 +36,27 @@ class ListKampanye extends StatelessWidget {
               itemBuilder: (context, index) {
                 final Campaign campaign =
                     Campaign.fromSnapshot(campaignDocs[index]);
-                return ListTile(
-                  title: Text(campaign.title),
-                  subtitle: Text(campaign.description),
-                  leading: campaign.imageUrl.isNotEmpty
-                      ? Image.network(
-                          campaign.imageUrl,
-                          width: 50,
-                          height: 50,
-                          fit: BoxFit.cover,
-                        )
-                      : null,
-                  trailing: ElevatedButton(
-                    onPressed: () => _joinKampanye(context, campaign.id),
-                    child: Text('Bergabung'),
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DetailKampanye(campaign: campaign),
+                      ),
+                    );
+                  },
+                  child: ListTile(
+                    title: Text(campaign.title),
+                    subtitle: Text(campaign.description),
+                    leading: campaign.imageUrl.isNotEmpty
+                        ? Image.network(
+                            campaign.imageUrl,
+                            width: 50,
+                            height: 50,
+                            fit: BoxFit.cover,
+                          )
+                        : null,
                   ),
                 );
               },
@@ -217,18 +68,149 @@ class ListKampanye extends StatelessWidget {
       ),
     );
   }
+}
 
-  void _joinKampanye(BuildContext context, String campaignId) async {
+// Halaman Detail
+class DetailKampanye extends StatefulWidget {
+  final Campaign campaign;
+
+  DetailKampanye({required this.campaign});
+
+  @override
+  _DetailKampanyeState createState() => _DetailKampanyeState();
+}
+
+class _DetailKampanyeState extends State<DetailKampanye> {
+  bool isJoined = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkIfUserJoined();
+  }
+
+  void checkIfUserJoined() async {
     final User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      // User belum login, lakukan penanganan sesuai kebutuhan aplikasi
-      return;
-    }
+    if (user != null) {
+      final String userId = user.uid;
 
+      final DocumentSnapshot campaignSnapshot = await FirebaseFirestore.instance
+          .collection('campaigns')
+          .doc(widget.campaign.id)
+          .get();
+
+      if (campaignSnapshot.exists) {
+        final Campaign campaign = Campaign.fromSnapshot(campaignSnapshot);
+
+        if (campaign.participants
+            .any((participant) => participant.uid == userId)) {
+          setState(() {
+            isJoined = true;
+          });
+        }
+      }
+    }
+  }
+
+  void joinCampaign() async {
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      showDialog(
+        context: context,
+        builder: (context) => Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 30,
+                  child: Icon(
+                    Icons.calendar_month,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                Text(
+                  'Anda yakin ingin bergabung dengan kampanye ini?',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.inter(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        width: 100,
+                        height: 45,
+                        child: ElevatedButton(
+                          child: Text(
+                            'Batal',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xFFF1F1F1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 16.0),
+                    Expanded(
+                      child: SizedBox(
+                        width: 100,
+                        height: 45,
+                        child: ElevatedButton(
+                          child: Text(
+                            'Yakin',
+                            style: TextStyle(fontWeight: FontWeight.w700),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                            _proceedToJoinCampaign(user);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xFFEC407A),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+  }
+
+  void _proceedToJoinCampaign(User user) async {
     final String userId = user.uid;
 
-    final DocumentReference campaignRef =
-        FirebaseFirestore.instance.collection('campaigns').doc(campaignId);
+    final DocumentReference campaignRef = FirebaseFirestore.instance
+        .collection('campaigns')
+        .doc(widget.campaign.id);
 
     final DocumentSnapshot campaignSnapshot = await campaignRef.get();
 
@@ -254,12 +236,64 @@ class ListKampanye extends StatelessWidget {
           'participants':
               campaign.participants.map((user) => user.toMap()).toList()
         });
+
+        setState(() {
+          isJoined = true;
+        });
       }
     }
+  }
 
-    // Tambahkan logika untuk pengiriman sertifikat oleh admin jika diperlukan
-
-    Navigator.pop(
-        context); // Kembali ke halaman sebelumnya setelah bergabung dalam kampanye
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.campaign.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.campaign.title,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            Image.network(widget.campaign.imageUrl),
+            SizedBox(height: 16),
+            Text(
+              'Deskripsi',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(widget.campaign.description),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: isJoined ? null : joinCampaign,
+              child: Text(
+                isJoined ? 'Sudah Bergabung' : 'Bergabung',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.disabled)) {
+                      return Colors.green;
+                    }
+                    return Colors.blue; // Use the component's default.
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
