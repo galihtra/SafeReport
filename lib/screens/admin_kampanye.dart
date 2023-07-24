0df32b9b6dbd1fcb65071f448da807c11471ddc4
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-07-24 16:02:48 +0700

    revisi ui admin kampanye

--- lib/screens/admin_kampanye.dart
+++ lib/screens/admin_kampanye.dart
@@ -105,6 +105,8 @@ class _KampanyeScreenState extends State<KampanyeScreen> {
                             borderRadius: BorderRadius.only(
                               topLeft: Radius.circular(8),
                               bottomLeft: Radius.circular(8),
+                              bottomRight: Radius.circular(8),
+                              topRight: Radius.circular(8),
                             ),
                           ),
                           child: Text(
